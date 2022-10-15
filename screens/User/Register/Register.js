00ecb34486be040b72321b5f4163016aa21d1526
yschristian <yubahwesc@gmail.com>
2022-10-15 16:32:32 +0200

    user Register

--- screens/User/Register/Register.js
+++ screens/User/Register/Register.js
@@ -2,35 +2,35 @@ import React from "react";
 import { Text, View, TextInput, Pressable } from "react-native";
 import styles from "./styles";
 
-const Register = () =>{
-    return(
+const Register = () => {
+    return (
         <View style={styles.container}>
-            <Text 
+            <Text
                 style={styles.text}
-                >Register New Partner
-                </Text>
+            >Register New Partner
+            </Text>
             <View style={styles.userLogin}>
                 <TextInput
                     style={styles.userText}
                     placeholder="UserName"
                 />
                 <TextInput
-                   style={styles.userText}
+                    style={styles.userText}
                     placeholder="Email"
                 />
-                 <TextInput
-                   style={styles.userText}
+                <TextInput
+                    style={styles.userText}
                     placeholder="Password"
                 />
                 <Pressable style={styles.button}>
                     <Text style={styles.buttonText}>LOGIN</Text>
                 </Pressable>
-               <View style={styles.row}>
-                <Text>
-                 Are you Our Patner? if not ,
-                 </Text> 
-                <Text style={styles.textRequest}>Request</Text>
-                </View> 
+                <View style={styles.row}>
+                    <Text>
+                        Are you Our Patner? if not ,
+                    </Text>
+                    <Text style={styles.textRequest}>Request</Text>
+                </View>
                 <Pressable style={styles.butn}>
                     <Text style={styles.forgotButton}>Forgot Password?</Text>
                 </Pressable>

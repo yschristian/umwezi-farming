ce3addd15ea2c4664cfca68ae2b11c381831119a
yschristian <yubahwesc@gmail.com>
2022-11-20 22:08:39 +0200

    drawer navigation

--- components/CustomDrawer.js
+++ components/CustomDrawer.js
@@ -1,14 +1,31 @@
-import {View, Text ,Image,ImageBackground ,StyleSheet} from "react-native"
+import {View, Text ,Image,ImageBackground ,StyleSheet, TouchableOpacity} from "react-native"
 import { DrawerContentScrollView, DrawerItemList } from "@react-navigation/drawer"
+ 
 const CustomDrawer =(props) =>{
     return(
         <View style={{flex:1}}>
              <DrawerContentScrollView {...props} contentContainerStyle={{backgroundColor:"green"}}>
-                <ImageBackground />
-            <DrawerItemList {...props}/>
+                <ImageBackground
+                 source={require("../assets/images/avatar2.jpg")}
+                 style={{padding:20}}
+                 >
+                    <Image 
+                        source={require("../assets/images/apple.jpg")}
+                        style={{height:80,width:80, borderRadius: 40, marginBottom: 10}}
+                        />
+                      <Text style={{color:"white", fontSize:18}}>Chris Farmer</Text>  
+                </ImageBackground>
+                <View style={{flex:1, backgroundColor:"white", paddingTop:10}}>
+                <DrawerItemList {...props}/>
+                </View>
             </DrawerContentScrollView>
-            <View>
-                <Text>Our custom text</Text>
+            <View style={{padding:20,borderTopWidth:1, borderTopColor:"#ccc"}}>
+                <TouchableOpacity style={{paddingVertical:15}}>
+                    <View>
+                    <Text>Our custom text</Text>
+                    </View>
+                </TouchableOpacity>
+               
             </View>
         </View>
       

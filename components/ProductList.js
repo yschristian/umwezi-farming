effcf22dd449a066530d0e851258eeb97a1b5585
yschristian <yubahwesc@gmail.com>
2022-11-23 17:55:47 +0200

    product list

--- components/ProductList.js
+++ components/ProductList.js
@@ -1,30 +1,38 @@
 import React from "react";
-import {View, Text ,Image ,StyleSheet,TouchableOpacity} from "react-native"
+import { View, Text, Image, StyleSheet, TouchableOpacity, FlatList } from "react-native"
 import { Ionicons } from '@expo/vector-icons';
+import fruits from "../assets/data/fruits"
+import { useNavigation } from "@react-navigation/native";
 
-// import fruits from "../assets/data/fruits";
-
-
-
-const ProductList = () =>{
-    return(
+const ProductList = () => {
+    const navigation = useNavigation()
+    return (
         <View style={styles.container}>
             <View style={styles.viewCont}>
-                <Text>Recommended Products</Text>
-                <TouchableOpacity >
-                    <Text>View All</Text>
+                <Text style={styles.recommanded}>Recommended Products</Text>
+                <TouchableOpacity onPress={()=> navigation.navigate("Recommanded")}>
+                    <Text style={styles.recommanded}>View All</Text>
                 </TouchableOpacity>
             </View>
-            <View style={styles.card} >
-                <Image 
-                style={styles.image}
-                 source={{uri:"https://static.vecteezy.com/system/resources/previews/001/937/723/original/rice-package-mockup-thailand-food-products-illustration-vector.jpg"}}
-                 />
-                 <Text style={styles.text1}>Rice</Text>
-                 <Text style={styles.text}>$ 100</Text>
-                 <Ionicons name="add-circle-outline" style={styles.icon} size={30} color="green" />
-             </View>
-             <View style={styles.card}>
+            <FlatList
+                data={fruits}
+                keyExtractor={(item,index) => item.id}
+                renderItem={({item}) => (
+                  <View style={styles.cardContainer}> 
+                    <View style={styles.card} key={item.id}>
+                        <Image
+                            style={styles.image}
+                            source={{ uri: item.image }}
+                        />
+                        <Text style={styles.text1}>{item.title}</Text>
+                        <Text style={styles.text}>$ {item.price}</Text>
+                        <Ionicons name="add-circle-outline" style={styles.icon} size={30} color="green" />
+                    </View>
+                    </View> 
+                )}
+            />
+
+            {/* <View style={styles.card}>
                 <Image 
                 style={styles.image}
                  source={{uri:"https://www.collinsdictionary.com/images/full/apple_158989157.jpg"}}
@@ -32,8 +40,8 @@ const ProductList = () =>{
                  <Text style={styles.text1}>Fruits</Text>
                  <Text style={styles.text}>$ 20</Text>
                  <Ionicons name="add-circle-outline" style={styles.icon} size={30} color="green" />
-             </View>
-             <View style={styles.card}>
+             </View> */}
+            {/* <View style={styles.card}>
                 <Image 
                 style={styles.image}
                  source={{uri:"https://www.wheafree.com/wp-content/uploads/2020/11/maize-floor-1.jpg"}}
@@ -41,8 +49,8 @@ const ProductList = () =>{
                  <Text style={styles.text1}>Fruits</Text>
                  <Text style={styles.text}>$ 20</Text>
                  <Ionicons name="add-circle-outline" style={styles.icon} size={30} color="green" />
-             </View>
-             <View style={styles.card}>
+             </View> */}
+            {/* <View style={styles.card}>
                 <Image 
                 style={styles.image}
                  source={{uri:"https://www.barrypackaging.com/media/wysiwyg/custom-paper-potato-bags-ireland.jpg"}}
@@ -50,7 +58,7 @@ const ProductList = () =>{
                  <Text style={styles.text1}>Irish Potato</Text>
                  <Text style={styles.text}>$ 20</Text>
                  <Ionicons name="add-circle-outline" style={styles.icon} size={30} color="green" />
-             </View>
+             </View> */}
         </View>
     )
 }
@@ -58,49 +66,59 @@ const ProductList = () =>{
 export default ProductList
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        marginTop:50,
+    container: {
+        flex: 1,
+        marginTop: 50,
         // marginHorizontal: 20,
-        flexDirection:"row",
-        flexWrap:"wrap",
+        // flexDirection: "row",
+        // flexWrap: "wrap",
         // backgroundColor:"blue"
     },
-    card:{
-        marginTop:15,
-        marginBottom:50,
-        backgroundColor:"white",
-        width:172,
-        height:200,
+    cardContainer:{
+        flex: 1,
+        flexDirection: "row",
+    },
+    card: {
+        marginTop: 15,
+        marginBottom: 50,
+        backgroundColor: "white",
+        width: 170,
+        height: 200,
         marginHorizontal: 12,
-        borderRadius:15,
+        borderRadius: 15,
         // marginVertical: 50,
     },
-    image:{
-        width:100,
-        height:100,
+    image: {
+        width: 100,
+        height: 100,
         top: -30,
-        marginHorizontal:40,
-        
+        marginHorizontal: 40,
+
     },
-    text:{
-        marginLeft:30,
-        fontSize:18,
-        fontWeight:"bold"
+    text: {
+        marginLeft: 30,
+        fontSize: 18,
+        fontWeight: "bold"
     },
-    text1:{
-        marginLeft:30,
-        fontSize:18,
+    text1: {
+        marginLeft: 30,
+        fontSize: 18,
     },
-    icon:{
-        left:130,
+    icon: {
+        left: 130,
         top: 0
     },
 
-    viewCont:{
-        backgroundColor:"red",
-        width:200,
-        marginBottom:50,
-        flexDirection:"row",
+    viewCont: {
+       
+        width: 200,
+        marginBottom: 50,
+        flexDirection: "row",
+        justifyContent:"space-between"
+    },
+    recommanded:{
+        fontSize:15,
+        color:"green",
+        marginRight: 60
     }
 })

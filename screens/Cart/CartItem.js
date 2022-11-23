a42283d776fef967aaf6b7a694d811641cdd7b5c
yschristian <yubahwesc@gmail.com>
2022-11-23 20:23:57 +0200

    cart items

--- screens/Cart/CartItem.js
+++ screens/Cart/CartItem.js
@@ -1,19 +1,26 @@
-import React from "react";
-import { StyleSheet, Text, View, Image,TouchableOpacity,FlatList } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Image, TouchableOpacity, FlatList } from 'react-native';
 import { Entypo, AntDesign } from '@expo/vector-icons';
 
 
-const CartItems = ({route}) => {
+const CartItems = () => {
+    const [quantity, setQuantity] = useState(0)
     const image = route.params.image
     const price = route.params.price
-    console.log(image,price);
-    const total = price *2
+    console.log(image, price);
+    const total = price * 2
+    const handleQuantity =(type)=>{
+        if(type==="dec"){
+          quantity >= 1 && setQuantity(quantity - 1)
+        }else{
+            setQuantity(quantity + 1)
+        }
+    }
     return (
         <View style={styles.container}>
             <View style={styles.orderTotal}>
-                <Text style={styles.total}>You save total of: {total} on this order</Text>
+                <Text style={styles.total}>You save total of: total on this order</Text>
             </View>
-            <FlatList />
             <View style={styles.preCont}>
                 <View style={styles.listItem}>
                     <View style={styles.body}>
@@ -22,7 +29,7 @@ const CartItems = ({route}) => {
                                 <Text style={styles.text}>apple</Text>
                             </View>
                             <View style={styles.textComp}>
-                                <Text style={styles.text}>${price}</Text>
+                                <Text style={styles.text}>$ 12</Text>
                             </View>
                             <View style={styles.textComp}>
                                 <Text style={styles.text}>size:50kg</Text>
@@ -40,9 +47,14 @@ const CartItems = ({route}) => {
                     </View>
                 </View>
                 <View style={styles.iconCont}>
-                    <Entypo name="plus" size={24} color="black" />
-                    <Text>Quantity </Text>
+                    <TouchableOpacity onPress={()=> handleQuantity("inc")}>
+                        <Entypo name="plus" size={24} color="black" />
+                    </TouchableOpacity>
+                    <Text style={styles.quantity}>{quantity}</Text>
+                    <TouchableOpacity onPress={()=> handleQuantity('dec')}>
                     <AntDesign name="minus" size={24} color="black" />
+                    </TouchableOpacity>
+                    
                 </View>
             </View>
             {/* <View style={styles.preCont}>
@@ -134,31 +146,35 @@ const styles = StyleSheet.create({
         borderBottomWidth: 1,
 
     },
-    checkoutContainer:{
-        bottom:0,
-        marginTop:20,
-        borderRadius:8,
+    checkoutContainer: {
+        bottom: 0,
+        marginTop: 20,
+        borderRadius: 8,
         marginHorizontal: 12,
-        backgroundColor:"green",
-        width:100,
-        height:40,
-        alignItems:"center",
-        justifyContent:"center"
+        backgroundColor: "green",
+        width: 100,
+        height: 40,
+        alignItems: "center",
+        justifyContent: "center"
     },
-    checkout:{
-        left:2,
-        color:"white",
-        fontSize:18,
-        fontWeight:"bold"
+    checkout: {
+        left: 2,
+        color: "white",
+        fontSize: 18,
+        fontWeight: "bold"
     },
-    orderTotal:{
-        marginTop:20,
-        marginLeft:10
+    orderTotal: {
+        marginTop: 20,
+        marginLeft: 10
     },
-    total:{
-        fontSize:15,
-        fontWeight:"bold"
-
+    total: {
+        fontSize: 15,
+        fontWeight: "bold"
+    },
+    quantity: {
+        fontSize: 18,
+        justifyContent: "center",
+        alignItems: "center"
     }
 
 

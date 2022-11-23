4718b31144bec06c4082ec8ae39dc1a8db11ade0
yschristian <yubahwesc@gmail.com>
2022-11-23 17:55:02 +0200

    new features created

--- assets/data/fruits.js
+++ assets/data/fruits.js
@@ -2,25 +2,19 @@ export default [
     {
         id:"0",
         image:"https://www.meijer.com/content/dam/meijer/product/0002/40/0005/98/0002400005988_1_A1C1_1200.png",
-        price:"$ 12",
-        title:"banana"
+        price:"12",
+        title:"Pineapple"
     },
     {
         id:"1",
         image:"https://www.collinsdictionary.com/images/full/apple_158989157.jpg",
-        price:"$ 20",
-        title:"banana"
+        price:"20",
+        title:"Apple"
     },
     {
         id:"2",
         image:"https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Oranges_-_whole-halved-segment.jpg/1200px-Oranges_-_whole-halved-segment.jpg",
-        price:"$23",
-        title:"banana"
+        price:"23",
+        title:"Orange"
     },
-    {
-        id:"3",
-        image:"https://images.news18.com/ibnlive/uploads/2022/03/watermelon.jpg",
-        price:"$12",
-        title:"banana"
-    }
 ]

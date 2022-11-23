dafe3b630e0352cca229b82895c5967c6874de5b
yschristian <yubahwesc@gmail.com>
2022-11-23 21:55:51 +0200

    my home

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
   useEffect(() => {
     setImages([
       "https://www.collinsdictionary.com/images/full/apple_158989157.jpg",
-      "https://www.collinsdictionary.com/images/full/apple_158989157.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Oranges_-_whole-halved-segment.jpg/1200px-Oranges_-_whole-halved-segment.jpg",
       "https://domf5oio6qrcr.cloudfront.net/medialibrary/6372/202ebeef-6657-44ec-8fff-28352e1f5999.jpg"
     ])
     return () => {

1257b91d29fd9dc28c8c871246fa97cc885b14a7
yschristian <yubahwesc@gmail.com>
2022-11-27 20:20:56 +0200

    my orders

--- screens/MyOrder/Order.js
+++ screens/MyOrder/Order.js
@@ -1,19 +1,35 @@
-import {View, Text ,Image ,StyleSheet} from "react-native"
+import { useNavigation } from "@react-navigation/native"
+import { View, Text, Image, StyleSheet } from "react-native"
 import { TouchableOpacity } from "react-native-gesture-handler"
-const Order =()=>{
-    return(
+const Order = () => {
+    const Navigation = useNavigation()
+    return (
         <View style={styles.container}>
-            <Text>no Records Found</Text>
-            <TouchableOpacity>
-                <Text>Shop Now</Text>
-            </TouchableOpacity>
+            <View style={styles.ordermessage}>
+
+                <Text style={{ fontSize: 15 }}>no Records Found!</Text>
+                <TouchableOpacity onPress={Navigation.navigate("Recommanded")}>
+                    <Text style={styles.textbutton}>Shop Now</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
 export default Order
 
 const styles = StyleSheet.create({
-    container:{
-        
+    container: {
+        width: "100%",
+        height: "100%"
+    },
+    ordermessage: {
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: "100%"
+    },
+    textbutton: {
+        color: "red",
+        marginTop: 12,
+        fontWeight: "bold"
     }
 })

225cd305415d58c37e0b38bc92cbd1b8019937f5
yschristian <yubahwesc@gmail.com>
2022-12-02 18:47:55 +0200

    cart redux

--- Redux/CartRedux.js
+++ Redux/CartRedux.js
@@ -2,16 +2,15 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const cartSlice =  createSlice({
     name: "cart",
-    initialState:{
-        products:[],
-        quantity:0,
-        total:0
-    },
+    initialState,
+        // products:[],
+        // quantity:0,
+        // total:0
     reducers:{
         addProduct:(state,action)=>{
-            state.quantity += 1;
-            state.products.push(action.payload);
-            state.total += action.payload.product?.price * action.payload.quantity;
+            // state.quantity += 1;
+            state.push(action.payload);
+            // state.total += action.payload.product?.price * action.payload.quantity;
            
         }
     }

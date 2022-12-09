9d155ec6ab9d53d5f1c4d9b7da27e36c84f254d4
yschristian <yubahwesc@gmail.com>
2022-12-09 21:16:00 +0200

    auth stack

--- screens/Navigation/AuthStack.js
+++ screens/Navigation/AuthStack.js
@@ -9,26 +9,34 @@ const Drawer = createDrawerNavigator();
 
 const AuthStack = () => {
     return (
-        <Drawer.Navigator drawerContent={props => <CustomDrawer {...props} />}  >
+        <Drawer.Navigator drawerContent={props => <CustomDrawer {...props} />}
+
+            // screenOptions={{
+            //     headerShown: false,
+            // }}
+            >
             <Drawer.Screen name="Home" component={HomeTabNavigator} options={{
                 drawerIcon: ({ color }) => (
                     <FontAwesome name="home" size={28} color={color} />
-                )
+                ),
+                headerShown: false,
+            
             }}
-            screenOptions={{ headerShown: false }}
-             />
+
+            />
             <Drawer.Screen name="My order" component={Order}
                 options={{
                     drawerIcon: ({ color }) => (
                         <Feather name="package" size={24} color={color} />
                     )
                 }}
-                 />
+            />
             <Drawer.Screen name="Product" component={Product}
                 options={{
                     drawerIcon: ({ color }) => (
                         <FontAwesome name="product-hunt" size={24} color={color} />
                     )
+                        
                 }}
                 screenOptions={{ headerShown: false }} />
         </Drawer.Navigator>
